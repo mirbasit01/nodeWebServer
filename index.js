d515c3d2f8aab87ecbd34d8cff56d119786b1324
mirbasit01 <mirt11477@gmail.com>
2025-12-23 18:02:33 +0500

    Handling URL's in NodeJS

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const http = require('http');
 const fs = require('fs');
+const url = require('url');
 
 
 const myserver = http.createServer((req, res) => {
+    if(req.url === '/favicon.ico') return; res.end();
     const log = `${Date.now()}: ${req.url} New Request Received\n`;
+    const myurl = url.parse(req.url, true);
+    console.log(myurl)
     fs.appendFile('log.txt', log, (err, data) => {
-        switch(req.url){
+        switch(myurl.pathname){
             case '/': res.end('Home Page');
             break;
-            case '/about': res.end('About Page');
+            case '/about': 
+            const username = myurl.query.myname;
+            console.log(username);
+            res.end(`About Page. Hello, ${username}`);
             break;
+            case '/search':
+                const search = myurl.query.search_query;
+                res.end(`You searched for: ${search}`);
             default: res.end('404 Page Not Found');
             break;
         }
